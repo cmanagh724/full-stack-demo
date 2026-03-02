ae2faca430cd7ed122015717dc98e4ebeedc77ca
Zacharie Montreuil <zmontreuil@rrc.ca>
2026-03-02 11:41:43 -0600

    Add new prisma configuration, client

--- apps/backend/prisma/migrations/20260302171520_init/migration.sql
+++ apps/backend/prisma/migrations/20260302171520_init/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "Term" (
     "id" SERIAL NOT NULL,
     "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "definition" TEXT NOT NULL,
     "isFavourite" BOOLEAN NOT NULL,
 
     CONSTRAINT "Term_pkey" PRIMARY KEY ("id")
--- apps/backend/src/api/v1/services/termService.ts
+++ apps/backend/src/api/v1/services/termService.ts
@@ -1,7 +1,7 @@
 // Use the term type defined in prisma/schema.prisma
 import { Term } from "@prisma/client";
+import { prisma } from "../../../../lib/prisma";
 // initialize a prisma client if not already and use in queries here
-import prisma from "../../../../prisma/client";
 
 /**
  * Services access data as necessary from the Prisma client. They invoke
